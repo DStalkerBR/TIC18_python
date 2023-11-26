82a49ab78b7bd60249e67b23e06721028d6a214c
DStalkerBR <jhon89araujo@gmail.com>
2023-11-26 00:12:22 -0300

    Implementa inserir e listar produtos

--- Jhonata/PP-P003/Supermercado/main.py
+++ Jhonata/PP-P003/Supermercado/main.py
@@ -1,24 +1,85 @@
+def limpar_tela():
+    import os
+    os.system('cls' if os.name == 'nt' else 'clear')
+    
+def pressionar_enter(mensagem = "Pressione ENTER para continuar..."):
+    input(mensagem)
+    
+def inserir_produto(produtos:list[dict]):
+    limpar_tela()
+    print("+---------------+----------------------+-----------+")
+    print("|                   Inserir produto                |")    
+    print("+---------------+----------------------+-----------+")
+
+    codigo = str(int(produtos[-1]['codigo']) + 1).zfill(13)
+    nome = input("Nome: ").capitalize()
+    preco = float(input("Preço: "))   
+
+    produtos.append({'codigo': codigo, 'nome': nome, 'preco': preco})
+    print("Produto inserido com sucesso!")
+    pressionar_enter()
+    
+def listar_produtos(produtos:list[dict]):    
+    inicio = 0
+    produtos_preco = sorted(produtos, key=lambda produto: produto['preco'])
+    while inicio < len(produtos):
+        limpar_tela()
+        print("+---------------+----------------------+-----------+")
+        print("|                   Lista de produtos              |")    
+        print("+---------------+----------------------+-----------+")
+        print("| Código        | Nome                 | Preço     |")
+        print("+---------------+----------------------+-----------+")
+
+        for produto in produtos_preco[inicio:inicio+11]:
+            preco = f"R${produto['preco']:.2f}"
+            print(f"| {produto['codigo']:<14}| {produto['nome']:<21}| {preco:<10}|")
+
+        print("+---------------+----------------------+-----------+")
+        inicio += 11
+        if inicio < len(produtos_preco):
+            pressionar_enter("Pressione ENTER para para ir para página...")
+        else:
+            pressionar_enter("Saindo... Pressione ENTER para continuar...")
+
 def main():
+    # inicia lista de dicionario     
+    # codigo pode ser string numero de 11 caracters
+    produtos:list[dict] = [{'codigo': '0000000000001', 'nome': 'Arroz', 'preco': 10.0}, \
+                            {'codigo': '0000000000002', 'nome': 'Feijão', 'preco': 8.0}, \
+                            {'codigo': '0000000000003', 'nome': 'Macarrão', 'preco': 5.0}, \
+                            {'codigo': '0000000000004', 'nome': 'Carne', 'preco': 25.0}, \
+                            {'codigo': '0000000000005', 'nome': 'Frango', 'preco': 15.0}, \
+                            {'codigo': '0000000000006', 'nome': 'Peixe', 'preco': 10.0}, \
+                            {'codigo': '0000000000007', 'nome': 'Ovo', 'preco': 5.0}, \
+                            {'codigo': '0000000000008', 'nome': 'Leite', 'preco': 3.0}, \
+                            {'codigo': '0000000000009', 'nome': 'Café', 'preco': 5.0}, \
+                            {'codigo': '0000000000010', 'nome': 'Açucar', 'preco': 3.0}, \
+                            {'codigo': '0000000000011', 'nome': 'Sal', 'preco': 2.0}, \
+                            {'codigo': '0000000000012', 'nome': 'Pão', 'preco': 5.0}]
     while(True):
-        # limpar_tela()
-        menu = "1. Inserir um produto\n" \
+        limpar_tela()
+        menu =  "Supermercado\n\n" \
+            "1. Inserir um produto\n" \
             "2. Excluir um produto\n" \
             "3. Listar todos os produtos\n" \
             "4. Consultar o preço de um produto\n" \
             "5. Sair\n"
 
         print(menu)
-        escolha = int(input("Escolha uma opção: "))
+        try:
+            escolha = int(input("Escolha uma opção: "))
+        except ValueError:
+            print("Opção inválida")
+            pressionar_enter()
+            continue
         match escolha:
             case 1:
-                print("Inserir um produto")
-                # inserir_produto(produto)
+                inserir_produto(produtos)
             case 2:
                 print("Excluir um produto")
                 # excluir_produto(codigo)
             case 3:
-                print("Listar todos os produtos")
-                 # listar_produtos()
+                listar_produtos(produtos)
             case 4:
                 print("Consultar o preço de um produto")
                 # consultar_preco(codigo)
@@ -27,7 +88,7 @@ def main():
                 break
             case _:
                 print("Opção inválida")
-                break
+                pressionar_enter()
 
 
 if __name__ == '__main__':
