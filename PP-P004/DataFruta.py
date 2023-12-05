fee1525f4d5b58893700337aec7637748e804910
DStalkerBR <jhon89araujo@gmail.com>
2023-12-05 03:24:57 -0300

    Add listarEmOrdem e classe Estatisticas

--- PP-P004/DataFruta.py
+++ PP-P004/DataFruta.py
@@ -95,12 +95,17 @@ class AnaliseDados(ABC):
     @abstractmethod
     def mostraMaior(self):
         pass
+    
+    @abstractmethod
+    def listarEmOrdem(self):
+        pass
 
 class ListaNomes(AnaliseDados):
     
     def __init__(self):
         super().__init__(type("String"))
         self.__lista = [] 
+        # self.__lista.extend(["João", "Maria", "José", "Ana", "Pedro", "Paulo", "Carlos", "Márcia"])
 
     def entradaDeDados(self):
         '''
@@ -124,20 +129,34 @@ class ListaNomes(AnaliseDados):
         meio = tamanho//2
         mediana = lista_ordenada[meio] if tamanho % 2 == 1 else lista_ordenada[meio-1]
         print("Mediana dos Nomes: ", mediana)
-        
 
     def mostraMenor(self):
         '''
         Este método retorna o menos elemento da lista
         '''
         print("Menor Nome: ", min(self.__lista))
-        
 
     def mostraMaior(self):
         '''
         Este método retorna o maior elemento da lista
         '''
         print("Maior Nome: ", max(self.__lista))
+        
+
+    def listarEmOrdem(self):
+        '''
+        Este método ordena a lista e mostra os elementos
+        '''
+        lista_ordenada = sorted(self.__lista)
+        print("Lista de Nomes: ", lista_ordenada)
+        
+    @property
+    def lista(self):
+        return self.__lista
+    
+    @lista.setter
+    def lista(self):
+        raise ValueError("Não é possível alterar a lista de nomes")
 
     def __str__(self):
         return str(self.__lista)
@@ -147,6 +166,7 @@ class ListaDatas(AnaliseDados):
     def __init__(self):
         super().__init__(type(Data))
         self.__lista = []  
+        # self.__lista.extend([ Data(15, 7, 2020),Data(3, 11, 2008),Data(22, 5, 2001),Data(8, 2, 2003),Data(12, 9, 2005),Data(1, 12, 2004)])
     
     def entradaDeDados(self):
         '''
@@ -185,6 +205,21 @@ class ListaDatas(AnaliseDados):
         Este método retorna o maior elemento da lista
         '''
         print("Maior Data: ", max(self.__lista))
+        
+    def listarEmOrdem(self):
+        '''
+        Este método ordena a lista e mostra os elementos
+        '''
+        lista_ordenada = sorted(self.__lista)
+        print("Lista de Datas: ", [str(data) for data in lista_ordenada])
+        
+    @property
+    def lista(self):
+        return self.__lista
+    
+    @lista.setter
+    def lista(self, lista):
+        self.__lista = lista
     
     def __str__(self):
         return str([str(data) for data in sorted(self.__lista)])
@@ -194,7 +229,9 @@ class ListaSalarios(AnaliseDados):
 
     def __init__(self):
         super().__init__(type(float))
-        self.__lista = []      
+        self.__lista = []   
+        # self.__lista.extend([1500.50, 2300.75, 1800.25, 2500.0, 2000.99, 3000.45])
+
 
     def entradaDeDados(self):
         '''
@@ -215,16 +252,11 @@ class ListaSalarios(AnaliseDados):
         Este método ordena a lista e mostra o
         elemento que está na metade da lista
         '''
-        # Se a lista tiver um número par de elementos
-        # a mediana é a média dos dois elementos do meio
-        # Se a lista tiver um número ímpar de elementos
-        # a mediana é o elemento do meio
         lista_ordenada = sorted(self.__lista)
         tamanho = len(lista_ordenada)
         meio = tamanho//2
         mediana = lista_ordenada[meio] if tamanho % 2 == 1 else (lista_ordenada[meio-1] + lista_ordenada[meio])/2
-        print("Mediana dos Salários: ", mediana)
-              
+        print("Mediana dos Salários: ", mediana)            
 
     def mostraMenor(self):
         '''
@@ -237,6 +269,21 @@ class ListaSalarios(AnaliseDados):
         Este método retorna o maior elemento da lista
         '''
         print("Maior Salário: ", max(self.__lista))
+        
+    def listarEmOrdem(self):
+        '''
+        Este método ordena a lista e mostra os elementos
+        '''
+        lista_ordenada = sorted(self.__lista)
+        print("Lista de Salários: ", lista_ordenada)
+        
+    @property
+    def lista(self):
+        return self.__lista
+    
+    @lista.setter
+    def lista(self, lista):
+        raise ValueError("Não é possível alterar a lista de salários")
     
     def __str__(self):
         return str(self.__lista)
@@ -246,6 +293,8 @@ class ListaIdades(AnaliseDados):
     def __init__(self):
         super().__init__(type(int))
         self.__lista = []    
+        #  self.__lista.extend([25, 30, 22, 35, 28, 40])
+
     
     def entradaDeDados(self):
         '''
@@ -282,10 +331,52 @@ class ListaIdades(AnaliseDados):
         Este método retorna o maior elemento da lista
         '''
         print("Maior Idade: ", max(self.__lista))
+        
+    def listarEmOrdem(self):
+        '''
+        Este método ordena a lista e mostra os elementos
+        '''
+        lista_ordenada = sorted(self.__lista)
+        print("Lista de Idades: ", lista_ordenada)
+        
+    @property
+    def lista(self):
+        return self.__lista
+    
+    @lista.setter
+    def lista(self, lista):
+        raise ValueError("Não é possível alterar a lista de idades")
 
     def __str__(self):
         return str(self.__lista)
 
+
+class Estatisticas:
+    def __init__(self, listaListas):
+        self.__lista = listaListas
+    
+    def nomeSalarios(self):        
+        print("Nome\t\tSalário")
+        print("-" * 25)
+        for nome, salario in zip(self.__lista[0].lista, self.__lista[2].lista):
+            print("{:<10}\t{:.2f}".format(nome, salario))
+    
+    def reajusteSalarios(self):
+        print("Custo da folha de pagamento:")
+        print(sum(self.__lista[2].lista))
+        print("Custo da folha de pagamento se os salários fossem reajustados em 10%:")
+        print(sum(map(lambda x: x*1.1, self.__lista[2].lista)))
+            
+    def modificaDatas(self):
+        datas_anteriores_2019 = filter(lambda x: x.ano < 2019, self.__lista[1].lista)
+        datas_modificadas = map(lambda x: Data(1, x.mes, x.ano), datas_anteriores_2019)
+        datas_posteriores_2019 = filter(lambda x: x.ano >= 2019, self.__lista[1].lista)
+        
+        self.__lista[1].lista = list(datas_modificadas) + list(datas_posteriores_2019)
+        print ("Datas anteriores a 2019 modificadas:")
+        for data in self.__lista[1].lista:
+            print(data)
+            
 def main():
     nomes = ListaNomes()
     datas = ListaDatas()
@@ -293,13 +384,27 @@ def main():
     idades = ListaIdades()
     
     listaListas = [nomes, datas, salarios, idades]
-
+    
+    estatisticas = Estatisticas(listaListas)
+    
     for lista in listaListas:
         lista.entradaDeDados()
+        lista.listarEmOrdem()
         lista.mostraMediana()
         lista.mostraMenor()
         lista.mostraMaior()
+        lista.listarEmOrdem()
+        
         print("___________________")
+        
+    
+    print("Estatisticas:")
+    estatisticas.nomeSalarios()
+    print("___________________")
+    estatisticas.reajusteSalarios()
+    print("___________________")
+    estatisticas.modificaDatas()
+    print("___________________")
 
     print("Fim do teste!!!")
 
